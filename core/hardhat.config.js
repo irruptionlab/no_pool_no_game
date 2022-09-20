7a050582a043454006c1a0c9921261bd94406d8f
Perrin <perrin.grandne@protonmail.com>
2022-09-20 11:50:31 +0200

    Check .env file not pushed

--- core/hardhat.config.js
+++ core/hardhat.config.js
@@ -1,4 +1,15 @@
+require("@nomicfoundation/hardhat-toolbox");
+require('dotenv').config();
+
+const ALCHEMY_API_KEY = process.env.ALCHEMY_ID;
+const GOERLI_PRIVATE_KEY = process.env.PRIVATE_KEY;
+
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.17",
+  solidity: "0.8.17", networks: {
+    goerli: {
+      url: `https://eth-goerli.alchemyapi.io/v2/${ALCHEMY_API_KEY}`,
+      accounts: [GOERLI_PRIVATE_KEY]
+    }
+  }
 };
