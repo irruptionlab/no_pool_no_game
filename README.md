abd3b4d21187c13d7bddc2b60747012742c33c42
Perrin <perrin.grandne@protonmail.com>
2022-09-20 11:16:20 +0200

    Adding core contracts

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # NoPoolNoGame
+This repository has 2 folders:
+- client for Front End
+- -core for Core Contracts
--- client/src/components/Timer.tsx
+++ client/src/components/Timer.tsx
@@ -5,7 +5,7 @@ const Timer = ({ isStarted }: { isStarted: boolean }) => {
 
     useEffect(() => {
         if (isStarted) {
-            setInterval(() => setMilliseconds(milliseconds + 1000), 1000)
+            setInterval(() => setMilliseconds(milliseconds + 1), 1)
         } else if (!isStarted && milliseconds !== 0) {
             setMilliseconds(0);
         }
@@ -14,7 +14,7 @@ const Timer = ({ isStarted }: { isStarted: boolean }) => {
 
     return (
         <div className="text-block-36">
-            {Math.floor(milliseconds / 60000)} : {Math.trunc(milliseconds / 1000)} : {milliseconds % 1000}
+            {Math.floor(milliseconds / 60000)} min : {milliseconds % 60000} ms
         </div>
 
     );
