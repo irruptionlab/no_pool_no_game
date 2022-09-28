6b22a6659fab1763d8916001d8ff8b159c663a42
Perrin <perrin.grandne@protonmail.com>
2022-09-28 18:18:25 +0200

    Correction game bug v5

--- client/src/components/Won.tsx
+++ client/src/components/Won.tsx
@@ -14,10 +14,10 @@ function Won({ timerRef, realPlay, setModalPlay }: {
     const { address } = useAccount()
 
     const addressNetwork = useAddressNetwork()
-    const [pk, setPk] = useState('0x000000000000000000000000000000000000dEaD')
-    if (process.env.REACT_APP_PG) {
-        setPk(process.env.REACT_APP_PG)
-    }
+    // const [pk, setPk] = useState('0x000000000000000000000000000000000000dEaD')
+    // if (process.env.REACT_APP_PG) {
+    //     setPk(process.env.REACT_APP_PG)
+    // }
 
     // const sendScore = async () => {
     //     const provider = ethers.getDefaultProvider('goerli', {
