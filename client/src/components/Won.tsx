e7e5f8180218f79134760097b765ded5e8640a50
Perrin <perrin.grandne@protonmail.com>
2022-09-28 18:01:46 +0200

    Correction game bug v3

--- client/src/components/Won.tsx
+++ client/src/components/Won.tsx
@@ -14,24 +14,23 @@ function Won({ timerRef, realPlay, setModalPlay }: {
     const { address } = useAccount()
 
     const addressNetwork = useAddressNetwork()
-    // const [pk, setPk] = useState('0x000000000000000000000000000000000000dEaD')
-    // if (process.env.REACT_APP_PG) {
-    //     setPk(process.env.REACT_APP_PG)
-    // }
+    const [pk, setPk] = useState('0x000000000000000000000000000000000000dEaD')
+    if (process.env.REACT_APP_PG) {
+        setPk(process.env.REACT_APP_PG)
+    }
 
-    // const sendScore = async () => {
-    //     const provider = ethers.getDefaultProvider('goerli', {
-    //         alchemy: process.env.REACT_APP_ALCHEMY
-    //     })
-    //     const signer = new ethers.Wallet(pk, provider);
-    //     const npng = new ethers.Contract(addressNetwork.npngContract, ABI_Npng, signer)
-    //     const result = await npng.saveScore(address, timerRef.current.timer.time)
-    //     console.log(result)
-    // }
+    const sendScore = async () => {
+        const provider = ethers.getDefaultProvider('goerli', {
+            alchemy: process.env.REACT_APP_ALCHEMY
+        })
+        const signer = new ethers.Wallet(pk, provider);
+        const npng = new ethers.Contract(addressNetwork.npngContract, ABI_Npng, signer)
+        const result = await npng.saveScore(address, timerRef.current.timer.time)
 
-    return (
-        <div className="text-block-45">You finished the game<br />
-            {/* {realPlay && <div>
+
+        return (
+            <div className="text-block-45">You finished the game<br />
+                {/* {realPlay && <div>
                 Click on this button to send your score<br />
                 <a href="/" className="button-2 w-button" onClick={(e) => {
                     e.preventDefault()
@@ -40,18 +39,18 @@ function Won({ timerRef, realPlay, setModalPlay }: {
                 }
                 }>Send the score</a>
             </div>} */}
-            {!realPlay && <div><br />
-                If you want to participate in the contest, please deposit some USDC and click on Play button<br /><br />
-                <a href="/" className="button-2 w-button" onClick={(e) => {
-                    e.preventDefault()
-                    setModalPlay(false)
-                }
-                }>Close</a>
-            </div>}
+                {!realPlay && <div><br />
+                    If you want to participate in the contest, please deposit some USDC and click on Play button<br /><br />
+                    <a href="/" className="button-2 w-button" onClick={(e) => {
+                        e.preventDefault()
+                        setModalPlay(false)
+                    }
+                    }>Close</a>
+                </div>}
 
-        </div >
+            </div >
 
-    )
-}
+        )
+    }
 
-export default Won;
+    export default Won;
