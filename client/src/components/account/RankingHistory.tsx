6be85df5f3236c6972c52bcf13ef61f719c48f65
Perrin <perrin.grandne@protonmail.com>
2022-10-11 15:08:02 +0200

    Adding colors for ranking history

--- client/src/components/account/RankingHistory.tsx
+++ client/src/components/account/RankingHistory.tsx
@@ -22,7 +22,8 @@ function RankingHistory({ setModalResult, setContest }:
 
     const displayResult = (rank: number, nbParticpants: number) => {
         if (rank === 0) {
-            return (<div className="grid-content-typo text-block-49" > No participation</div>)
+            return (<div className="text-block-49" > No participation</div>)
+
         }
         else {
             if (rank < 11) {
@@ -40,8 +41,30 @@ function RankingHistory({ setModalResult, setContest }:
         }
     }
 
-    return (
+    const colorText = (rank: number, nbParticpants: number) => {
+        rank = parseInt(ethers.utils.formatUnits(rank, 0));
+        nbParticpants = parseInt(ethers.utils.formatUnits(nbParticpants, 0));
+        if (rank === 0) {
+            return ("grid-content-typo no-particpation-grey-color")
 
+        }
+        else {
+            if (rank < 11) {
+                return ("grid-content-typo win-typo")
+            }
+            else {
+                const result = parseInt(ethers.utils.formatUnits(rank, 0)) / parseInt(ethers.utils.formatUnits(nbParticpants, 0));
+                if (result * 100 < 10) {
+                    return ("grid-content-typo top10-typo")
+                }
+                else {
+                    return ("")
+                }
+            }
+        }
+    }
+
+    return (
         <div className="div-block-46">
             <div className="column-names">Contest #</div>
             <div className="column-names">Ranking</div>
@@ -49,16 +72,18 @@ function RankingHistory({ setModalResult, setContest }:
             <div className="column-names">Results</div>
             {data && data.filter(element => parseInt(ethers.utils.formatUnits(element[0]._hex, 0)) > 0).map(filteredElement =>
                 <Fragment key={filteredElement}>
-                    <div className="grid-content-typo" onClick={() => {
-                        setContest(parseInt(ethers.utils.formatUnits(filteredElement[0]._hex, 0)))
-                        setModalResult(true)
-                    }}
-                    ># {ethers.utils.formatUnits(filteredElement[0]._hex, 0)}</div>
-                    <div className="grid-content-typo">
+
+                    <div className={colorText(filteredElement[1]._hex, filteredElement[2]._hex)}
+                        onClick={() => {
+                            setContest(parseInt(ethers.utils.formatUnits(filteredElement[0]._hex, 0)))
+                            setModalResult(true)
+                        }}
+                    >#{ethers.utils.formatUnits(filteredElement[0]._hex, 0)}</div>
+                    <div className={colorText(filteredElement[1]._hex, filteredElement[2]._hex)}>
                         {(ethers.utils.formatUnits(filteredElement[1]._hex, 0) === "0") ? "n/a" : ethers.utils.formatUnits(filteredElement[1]._hex, 0)}
                     </div>
 
-                    <div className="grid-content-typo">{ethers.utils.formatUnits(filteredElement[2]._hex, 0)}</div>
+                    <div className={colorText(filteredElement[1]._hex, filteredElement[2]._hex)}>{ethers.utils.formatUnits(filteredElement[2]._hex, 0)}</div>
                     {displayResult(parseInt(ethers.utils.formatUnits(filteredElement[1]._hex, 0)), parseInt(ethers.utils.formatUnits(filteredElement[2]._hex, 0)))}
                 </Fragment>)}
         </div>
@@ -66,70 +91,3 @@ function RankingHistory({ setModalResult, setContest }:
 }
 
 export default RankingHistory;
-
-/*
-                <div className="grid-content-typo">#2</div>
-                <div className="grid-content-typo">80</div>
-                <div className="grid-content-typo">219</div>
-                <div>You can do better...</div>
-                <div className="grid-content-typo win-typo">#1</div>
-                <div className="grid-content-typo win-typo">3</div>
-                <div className="grid-content-typo win-typo">90</div>
-                <div className="grid-content-typo win-typo">$86.00</div>
-                <div className="grid-content-typo no-particpation-grey-color">#0</div>
-                <div className="grid-content-typo no-particpation-grey-color">n/a</div>
-                <div className="grid-content-typo no-particpation-grey-color">122</div>
-                <div className="text-block-49">No participation</div>
-            </Fragment >)
-}
-    </div > */
-
-                // <div>
-                //     <div className="div-block-27" >
-                //         <ul className="column-title">
-                //             <li className="list-item-8">
-                //                 <div className="text-block-29">Contest #</div>
-                //             </li>
-                //         </ul>
-                //         <ul className="column-title">
-                //             <li key="0" className="list-item-8">
-                //                 <div className="text-block-29">Ranking</div>
-                //             </li>
-                //         </ul>
-                //         <ul className="column-title">
-                //             <li className="list-item-8">
-                //                 <div className="text-block-29">Participants</div>
-                //             </li>
-                //         </ul>
-                //         <ul className="column-title">
-                //             <li key="0" className="list-item-8">
-                //                 <div className="text-block-29">Results</div>
-                //             </li>
-                //         </ul>
-
-                //     </div>
-                //     {data && data.filter(element => parseInt(ethers.utils.formatUnits(element[0]._hex, 0)) > 0).map(filteredElement =>
-                //         <div key={filteredElement} className="div-block-27">
-                //             <ul className="column-content">
-                //                 <li className="list-item-1"
-                //                     onClick={() => {
-                //                         setContest(parseInt(ethers.utils.formatUnits(filteredElement[0]._hex, 0)))
-                //                         setModalResult(true)
-                //                     }}
-                //                 ># {ethers.utils.formatUnits(filteredElement[0]._hex, 0)}
-                //                 </li>
-                //             </ul>
-                //             <ul className="column-content">
-                //                 <li className="list-item-1"> {ethers.utils.formatUnits(filteredElement[1]._hex, 0)}</li>
-                //             </ul>
-                //             <ul className="column-content">
-                //                 <li className="list-item-1"> {ethers.utils.formatUnits(filteredElement[2]._hex, 0)}</li>
-                //             </ul>
-                //             <ul className="column-content">
-                //                 {displayResult(parseInt(ethers.utils.formatUnits(filteredElement[1]._hex, 0)), parseInt(ethers.utils.formatUnits(filteredElement[2]._hex, 0)))}
-                //             </ul>
-                //         </div>)
-                //     }
-                // </div >
-
-
